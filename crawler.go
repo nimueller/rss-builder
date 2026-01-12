38cbe7f941f0a930650b2d494a12c89ae7368f34
Nico Müller <mueller@cryptospace.dev>
2026-01-12 21:38:21 +0100

    replace hardcoded selectors with `ScrapTarget` struct for dynamic configuration and add content extraction logic.

--- crawler.go
+++ crawler.go
@@ -7,36 +7,70 @@ import (
 )
 
 type Item struct {
-	Title    string
-	ImageURL string
+	Title      string
+	ArticleURL string
+	ImageURL   string
+	Content    string
+}
+
+type ScrapTarget struct {
+	URL                    string
+	baseGoquerySelector    string
+	itemGoquerySelector    string
+	imageGoquerySelector   string
+	articleGoquerySelector string
 }
 
 func main() {
+	target := ScrapTarget{
+		URL:                    "https://kicker.de",
+		baseGoquerySelector:    "#kick__ressort > section:nth-child(2)",
+		itemGoquerySelector:    ".kick__slidelist__item",
+		imageGoquerySelector:   ".kick__slidelist__item_content_picture img",
+		articleGoquerySelector: "main.kick__article__content",
+	}
+
+	queryItems(target)
+}
+
+func queryItems(target ScrapTarget) {
 	collector := colly.NewCollector()
 
-	queryItems(collector, "#kick__ressort > section:nth-child(2)", ".kick__slidelist__item", ".kick__slidelist__item_content_picture")
+	collector.OnHTML(target.baseGoquerySelector, func(e *colly.HTMLElement) {
+		e.ForEach(target.itemGoquerySelector, func(_ int, itemElement *colly.HTMLElement) {
+			item := &Item{
+				Title:      itemElement.ChildText("a[title]"),
+				ArticleURL: itemElement.ChildAttr("a[title]", "href"),
+				ImageURL:   itemElement.ChildAttr(fmt.Sprintf("%s img", target.imageGoquerySelector), "src"),
+			}
+
+			context := colly.NewContext()
+			context.Put("item", item)
+			itemElement.Request.Ctx.Put("item", item)
+
+			err := itemElement.Request.Visit(item.ArticleURL)
+
+			if err != nil {
+				fmt.Println(err)
+			}
+		})
+	})
+
+	collector.OnHTML(target.articleGoquerySelector, func(e *colly.HTMLElement) {
+		item := e.Request.Ctx.GetAny("item").(*Item)
+		articleHtml, err := e.DOM.Html()
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		item.Content = articleHtml
+		fmt.Println(item.Content)
+	})
 
 	err := collector.Visit("https://kicker.de")
 
 	if err != nil {
 		panic(err)
 	}
-}
-
-func queryItems(collector *colly.Collector, baseGoquerySelector string, itemGoquerySelector string, imageSelector string) {
-	collector.OnHTML(baseGoquerySelector, func(e *colly.HTMLElement) {
-		e.ForEach(itemGoquerySelector, func(_ int, itemElement *colly.HTMLElement) {
-			item := Item{}
-			getItem(itemElement, &item, imageSelector)
-
-			println()
-			println("\t", item.Title)
-			println("\t", item.ImageURL)
-		})
-	})
-}
-
-func getItem(itemElement *colly.HTMLElement, item *Item, imageSelector string) {
-	item.Title = itemElement.ChildText("a[title]")
-	item.ImageURL = itemElement.ChildAttr(fmt.Sprintf("%s img", imageSelector), "src")
 }
